535eee82f2b2f42363f005072f6b02296cd536ef
Milo <milo.sulin@gmail.com>
2024-07-13 23:59:19 +0300

    Solution for day 4 both tasks

--- y2015cpp/day4/src/main.cpp
+++ y2015cpp/day4/src/main.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
-using namespace std;
+#include <string>
+#include <array>
+#include <sstream>
+#include <chrono>
+#include "md5.h"
+using std::string, std::cout, std::array, std::stringstream, std::milli;
+
+constexpr string key = {"yzbqklnj"};
+
+struct Results{
+    int task1;
+    int task2;
+};
+
+
+Results searchForZeroes(){
+    int task1{};
+    int task2{};
+    bool solved1 = false;
+    bool solved2 = false;
+    int trial = 0;
+    while(!solved1 || !solved2){
+        trial++;
+        stringstream buf;
+        buf << key << trial;
+        string input = buf.str();
+        string output = md5(input);
+        for (int i=0; i<6; ++i){
+            if(output[i] != '0')
+                break;
+            else if(i == 4 && !solved1){
+                task1 = trial;
+                solved1 = true;
+            }else if(i == 5 && !solved2){
+                task2 = trial;
+                solved2 = true;
+            }
+        }
+    }
+
+    return {task1, task2};
+}
+
+
 
 int main(){
-    char i = 0x80;
-    cout << "testi: " << i << '\n';
+    const auto start_t = std::chrono::high_resolution_clock::now();
+    auto [task1, task2] = searchForZeroes();
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, milli> elapsed_time{end_t - start_t};
+    cout << "Solution to task 1: " << task1 << '\n';
+    cout << "Solution to task 2: " << task2 << '\n';
+    cout << "Elapsed time: " << elapsed_time << std::endl;
     return 0;
 }
