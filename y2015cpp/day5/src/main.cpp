c87860af3b9349fa0f974fa755f1fef0c6d8d673
Milo <milo.sulin@gmail.com>
2024-07-15 00:17:53 +0300

    Day 5 task 1 solution

--- y2015cpp/day5/src/main.cpp
+++ y2015cpp/day5/src/main.cpp
@@ -1,11 +1,3 @@
-/* 
-RULES:
-1. Must contain three vowels
-2. Must contain one instance of a letter appearing twice in a row: "aa", "dd", "gg" etc.
-3. Cannot contain "ab", "cd", "pq", "xy" even if they are part of the other requirements
-
-Task 1: How many strings (lines) fulfill these requirements?
- */
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -16,6 +8,13 @@ Task 1: How many strings (lines) fulfill these requirements?
 #include <thread>
 using namespace std::chrono;
 using std::cout, std::valarray, std::array, std::ifstream, std::stringstream, std::string, std::vector;
+
+valarray<char> forbid1 = {'a', 'b'};
+valarray<char> forbid2 = {'c', 'd'};
+valarray<char> forbid3 = {'p', 'q'};
+valarray<char> forbid4 = {'x', 'y'};
+
+const string vowels = "aeiou";
 
 vector<valarray<char>> readInput(const string& filepath){
     ifstream reader(filepath); // read file into fstream
@@ -41,22 +40,63 @@ vector<valarray<char>> readInput(const string& filepath){
 
 int perfTaskOne(vector<valarray<char>>){
     int nice{}; // amount of nice strings
+
+
     return 1;
 }
 
+/* 
+RULES:
+1. Must contain three vowels
+2. Must contain one instance of a letter appearing twice in a row: "aa", "dd", "gg" etc.
+3. Cannot contain "ab", "cd", "pq", "xy" even if they are part of the other requirements
+
+Task 1: How many strings (lines) fulfill these requirements?
+ */
 
 int main(){
     const auto start_t = high_resolution_clock::now();
     // read input
     auto input = readInput("../input.txt");
-    const auto end_t = high_resolution_clock::now();
+    int nice{0};
+    //solve part 1
+    for (const auto& line : input){
+        bool double_letter{false}; // contains double letter
+        bool contains_forbid{false}; // contains forbid
+        int vowel_count{0}; // contains three vowels
+        for (int i=0; i<line.size()-1; ++i){
+            // check each char pair against forbidden sequences and return min element from result
+            // this will give the respective bool check value 0 if no forbidden sequence was found
+            bool f1_check = (valarray<bool>(line[std::slice(i,2,1)] == forbid1)).min();
+            bool f2_check = (valarray<bool>(line[std::slice(i,2,1)] == forbid2)).min();
+            bool f3_check = (valarray<bool>(line[std::slice(i,2,1)] == forbid3)).min();
+            bool f4_check = (valarray<bool>(line[std::slice(i,2,1)] == forbid4)).min();
 
-    for (const auto& e : input[0]){
-        cout << e;
+            if (f1_check | f2_check | f3_check | f4_check){
+                contains_forbid = true;
+                break;
+            }
+
+            if(line[i] == line[i+1]){
+                double_letter = true;
+            }
+        }
+        valarray<bool> vowel_arr = (line == valarray<char>(vowels[0], line.size()));
+        vowel_arr |= (line == valarray<char>(vowels[1], line.size()));
+        vowel_arr |= (line == valarray<char>(vowels[2], line.size()));
+        vowel_arr |= (line == valarray<char>(vowels[3], line.size()));
+        vowel_arr |= (line == valarray<char>(vowels[4], line.size()));
+        for (const auto& vow : valarray<bool>(vowel_arr[vowel_arr == true])){
+            vowel_count++;
+        }
+        if(double_letter && vowel_count >= 3 && !contains_forbid){
+            nice++;
+        }
     }
-    cout << '\n';
-
+    
+    const auto end_t = high_resolution_clock::now();
     duration<double, std::milli> elapsed_time{end_t - start_t};
+    cout << "Nice strings: " << nice << '\n';
     cout << "Time taken: " << elapsed_time << '\n';
     return 0;
 }
