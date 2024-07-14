b1ee3eecac3bb51895ffb41bfffb8a0164a033c8
Milo <milo.sulin@gmail.com>
2024-07-14 20:31:57 +0300

    day 5 file reading a bit faster

--- y2015cpp/day5/src/main.cpp
+++ y2015cpp/day5/src/main.cpp
@@ -23,19 +23,26 @@ vector<valarray<char>> readInput(const string& filepath){
     vector<valarray<char>> input;
 
     char temp[32]; // char array for line hoping that it's smaller than 32 chars
+    int line_length{};
+    int over_four{};
+    int unroll{};
 
     while(reader.getline(&temp[0], 32)){ // read line
-        int amount_extracted = reader.gcount(); // record how many chars were read
-        valarray<char> line(amount_extracted); // init valarray for line, should be faster to compare to rules using bitwise operators
-        int over_four = amount_extracted % 4;
+        if (line_length == 0){
+            line_length = reader.gcount();
+            over_four = line_length % 4;
+            unroll = line_length - over_four;
+        }
+         // record how many chars were read
+        valarray<char> line(line_length); // init valarray for line, should be faster to compare to rules using bitwise operators
         int i=0;
-        for (; i<amount_extracted-over_four; i+=4){ // from char array to valarray
+        for (; i<unroll; i+=4){ // from char array to valarray
             line[i] = temp[i];
             line[i+1] = temp[i+1];
             line[i+2] = temp[i+2];
             line[i+3] = temp[i+3];
         }
-        for (; i<amount_extracted; ++i){ // insert leftovers too
+        for (; i<line_length; ++i){ // insert leftovers too
             line[i] = temp[i];
         }
         input.push_back(line); // insert read line into input vector
@@ -43,15 +50,23 @@ vector<valarray<char>> readInput(const string& filepath){
     return input;
 }
 
+int perfTaskOne(vector<valarray<char>>){
+    int nice{}; // amount of nice strings
+    return 1;
+}
+
 
 int main(){
     const auto start_t = high_resolution_clock::now();
     // read input
     auto input = readInput("../input.txt");
-
-    const auto cpu_count = std::thread::hardware_concurrency();
-    cout << cpu_count << '\n';
     const auto end_t = high_resolution_clock::now();
+
+    for (const auto& e : input[0]){
+        cout << e;
+    }
+    cout << '\n';
+
     duration<double, std::milli> elapsed_time{end_t - start_t};
     cout << "Time taken: " << elapsed_time << '\n';
     return 0;
