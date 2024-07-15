a3c9de0dd4be0e3aa99e2962c76589dbf9fc560b
Milo <milo.sulin@gmail.com>
2024-07-15 20:21:28 +0300

    Day 6 beginning

--- y2015cpp/day5/src/main.cpp
+++ y2015cpp/day5/src/main.cpp
@@ -9,13 +9,10 @@
 #include <string>
 #include <mutex>
 #include <cmath>
+#include <cstring>
 using namespace std::chrono;
 using std::cout, std::array, std::ifstream, std::string, std::vector;
 
-const string forbid1 = "ab";
-const string forbid2 = "cd";
-const string forbid3 = "pq";
-const string forbid4 = "xy";
 
 const string vowels = "aeiou";
 const string alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -42,33 +39,34 @@ struct Results{
 void taskOneRules(vector<array<char, 32>>* lines, std::atomic<int>* nice){
     int nice_own{0};
     for (auto& line : *lines){
+        char* line_p = &line[0];
+
         bool double_letter{false}; // contains double letter
         bool contains_forbid{false}; // contains forbid
         int vowel_count{0}; // contains three vowels
         
-        string test{""};
         char* last_seen = nullptr; // pointer to char that preceded the current one
         for (auto& e : line){
-            if(!alphabet.contains(e)){
+            if(!alphabet.contains(*line_p)){
                 break;
             }
 
-            test += e;
-
-            if (vowels.contains(e)){
+            if (vowels.contains(*line_p)){
                 vowel_count++;
             }
 
-            if(last_seen != nullptr && *last_seen == e){
+            if(!double_letter && last_seen != nullptr && *last_seen == *line_p){
                 double_letter = true;
             }
-            last_seen = &e;
-        }
+            last_seen = line_p;
 
-        if(test.contains(forbid1) | test.contains(forbid2) | test.contains(forbid3) | test.contains(forbid4)){
-            contains_forbid = true;
+            if(std::memcmp(line_p, "ab", 2)==0 || std::memcmp(line_p, "cd", 2)==0 || std::memcmp(line_p, "pq", 2)==0 || std::memcmp(line_p, "xy", 2)==0 ){
+                contains_forbid = true;
+            }
+
+            line_p++;
+            
         }
-        
         if(double_letter && vowel_count >= 3 && !contains_forbid){
             nice_own++;
         }
@@ -80,47 +78,40 @@ void taskOneRules(vector<array<char, 32>>* lines, std::atomic<int>* nice){
 void taskTwoRules(vector<array<char, 32>>* lines, std::atomic<int>* nice){
     int nice_own{0};
     for (auto& line : *lines){
+        char* line_p = &line[0];
+
         bool repeat_letter{false}; // contains repeating letter
         bool repeat_pair{false}; // contains repeating pair
         
-        char* one_back = nullptr; // char preceding current one
         char* two_back = nullptr; // char two places behind current one
-
-        // string full_string{""}; // string to use for checking if string has repeating pair
-        // for (auto& e : line){
-        //     if(!alphabet.contains(e)){ // incoming char array has empty places, stop reading line once we run out of real symbols
-        //         break;
-        //     }
-        //     full_string += e;
-        // }
-
         int line_pos{0}; // index of current char
         uint64_t line_length{line.size()};
+
         for (auto& e : line){
-            if(!alphabet.contains(e)){ // incoming char array has empty places, stop reading line once we run out of real symbols
+            if(!alphabet.contains(*line_p)){ // incoming char array has empty places, stop reading line once we run out of real symbols
                 break;
             }
 
-            string test{""}; // construct pair to look for in the rest of the string
-            if (line_pos >= 1 && !repeat_pair){
-                test += *one_back;
-                test += e;
-                string sub_str = string(line.begin() +line_pos+1, line.begin() +line_length);
-                if ( sub_str.contains(test) ) // check if rest of string contains the current pair
-                    repeat_pair = true;
+            if(!repeat_pair){
+                for(int i=2; i<line_length-line_pos; ++i){
+                    if(std::memcmp(line_p, line_p+i, 2)==0){
+                        repeat_pair = true;
+                    }
+                }
             }
 
-            if(two_back != nullptr && one_back != nullptr && *two_back == e){
+            if(!repeat_letter && two_back != nullptr && *two_back == *line_p){
                 repeat_letter = true;
             }
-            two_back = one_back;
-            one_back = &e;
-
             line_pos++;
+            two_back = line_p-1;
+            line_p++;
+            if(repeat_letter && repeat_pair){
+                nice_own++;
+                break;
+            }
         }
-        if(repeat_letter & repeat_pair){
-            nice_own++;
-        }
+
     }
     *nice += nice_own;
 }
@@ -130,7 +121,7 @@ Results perfTasks(vector<array<char, 32>>* input){
     std::atomic<int>* nice1 = new std::atomic<int>{0};
     std::atomic<int>* nice2 = new std::atomic<int>{0};
 
-    int process_count = (std::thread::hardware_concurrency() / 2 +2);
+    int process_count = (std::thread::hardware_concurrency() -2);
     int lines_per_thread = 2* std::floor(line_count / (process_count-2));
     int left_over = line_count % lines_per_thread;
     std::jthread* thread_pool[process_count];
@@ -168,17 +159,21 @@ TASK 2 RULES:
  */
 
 int main(){
-    auto start_t = high_resolution_clock::now();
-    // read input
-    auto* input = new vector<array<char, 32>>{readInput("../input.txt")};
-    // perform task 1 and 2
-    auto [nice1, nice2] = perfTasks(input);
-    auto end_t = high_resolution_clock::now();
-    duration<double, std::milli> elapsed_time{end_t - start_t};
-
-    cout << "Nice strings in task 1: " << nice1 << '\n';
-    cout << "Nice strings in task 2: " << nice2 << '\n';
-    cout << "Time taken: " << elapsed_time << '\n';
-    input->clear();
+    duration<double, std::milli> sum{0};
+    for(int i=0; i<10000; ++i){
+        auto start_t = high_resolution_clock::now();
+        // read input
+        auto* input = new vector<array<char, 32>>{readInput("../input.txt")};
+        // perform task 1 and 2
+        auto [nice1, nice2] = perfTasks(input);
+        auto end_t = high_resolution_clock::now();
+        duration<double, std::milli> elapsed_time{end_t - start_t};
+        sum += elapsed_time;
+    }
+    cout << "Average time: " << sum/10000 << '\n';
+    // cout << "Nice strings in task 1: " << nice1 << '\n';
+    // cout << "Nice strings in task 2: " << nice2 << '\n';
+    // cout << "Time taken: " << elapsed_time << '\n';
+    // input->clear();
     return 0;
 }
