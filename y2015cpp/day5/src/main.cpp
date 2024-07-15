69a448facc06086e778e769c60c58fb26619cfcf
Milo <milo.sulin@gmail.com>
2024-07-15 03:30:15 +0300

    Day 5 task 1 down to ~1.8ms

--- y2015cpp/day5/src/main.cpp
+++ y2015cpp/day5/src/main.cpp
@@ -9,40 +9,64 @@
 using namespace std::chrono;
 using std::cout, std::valarray, std::array, std::ifstream, std::stringstream, std::string, std::vector;
 
-valarray<char> forbid1 = {'a', 'b'};
-valarray<char> forbid2 = {'c', 'd'};
-valarray<char> forbid3 = {'p', 'q'};
-valarray<char> forbid4 = {'x', 'y'};
+const string forbid1 = "ab";
+const string forbid2 = "cd";
+const string forbid3 = "pq";
+const string forbid4 = "xy";
 
 const string vowels = "aeiou";
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-vector<valarray<char>> readInput(const string& filepath){
+vector<array<char, 32>> readInput(const string& filepath){
     ifstream reader(filepath); // read file into fstream
     
-    vector<valarray<char>> input;
+    vector<array<char, 32>> input;
 
-    char temp[32]; // char array for line hoping that it's smaller than 32 chars
-    int line_length{};
+    array<char, 32> line; // char array for line hoping that it's smaller than 32 chars
 
-    while(reader.getline(&temp[0], 32)){ // read line
-        if (line_length == 0){
-            line_length = reader.gcount(); // record how many chars were read and assume all lines are the same
-        }
-        
-        valarray<char> line(line_length); // init valarray for line, should be faster to compare to rules using bitwise operators
-        for (int i=0; i<line_length; ++i){ // from char array to valarray
-            line[i] = temp[i];
-        }
+    while(reader.getline(&line[0], 32)){ // read line
         input.push_back(line); // insert read line into input vector
     }
     return input;
 }
 
-int perfTaskOne(vector<valarray<char>>){
-    int nice{}; // amount of nice strings
 
+int perfTaskOne(vector<array<char, 32>>* input){
+    int nice{0};
+    //solve part 1
+    for (auto& line : *input){
+        bool double_letter{false}; // contains double letter
+        bool contains_forbid{false}; // contains forbid
+        int vowel_count{0}; // contains three vowels
+        
+        string test{""};
+        char* last_seen = NULL;
+        for (auto& e : line){
+            if(!alphabet.contains(e)){
+                break;
+            }
 
-    return 1;
+            test += e;
+
+            if (vowels.contains(e)){
+                vowel_count++;
+            }
+
+            if(last_seen != NULL && *last_seen == e){
+                double_letter = true;
+            }
+            last_seen = &e;
+        }
+
+        if(test.contains(forbid1) | test.contains(forbid2) | test.contains(forbid3) | test.contains(forbid4)){
+            contains_forbid = true;
+        }
+        
+        if(double_letter && vowel_count >= 3 && !contains_forbid){
+            nice++;
+        }
+    }
+    return nice;
 }
 
 /* 
@@ -57,41 +81,12 @@ Task 1: How many strings (lines) fulfill these requirements?
 int main(){
     const auto start_t = high_resolution_clock::now();
     // read input
-    auto input = readInput("../input.txt");
-    int nice{0};
-    //solve part 1
-    for (const auto& line : input){
-        bool double_letter{false}; // contains double letter
-        bool contains_forbid{false}; // contains forbid
-        int vowel_count{0}; // contains three vowels
-        for (int i=0; i<line.size()-1; ++i){
-            std::slice area_to_check(i, 2, 1);
-            if((line[area_to_check] == forbid1).min() || (line[area_to_check] == forbid2).min()
-            || (line[area_to_check] == forbid3).min() || (line[area_to_check] == forbid4).min()){
-                contains_forbid = true;
-                break;
-            }
-
-            if (vowels.contains(line[i])){
-                vowel_count++;
-            }
-
-            if(line[i] == line[i+1]){
-                double_letter = true;
-            }
-        }
-        if (vowels.contains(line[line.size()])){
-            vowel_count++;
-        }
-        
-        if(double_letter && vowel_count >= 3 && !contains_forbid){
-            nice++;
-        }
-    }
-    
+    auto* input = new vector<array<char, 32>>{readInput("../input.txt")};
+    // perform task 1
+    int nice1 = perfTaskOne(input);
     const auto end_t = high_resolution_clock::now();
     duration<double, std::milli> elapsed_time{end_t - start_t};
-    cout << "Nice strings: " << nice << '\n';
+    cout << "Nice strings: " << nice1 << '\n';
     cout << "Time taken: " << elapsed_time << '\n';
     return 0;
 }
