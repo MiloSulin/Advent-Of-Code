9dd15bdb2534156ae7720cc95d94da539157507b
Milo <milo.sulin@gmail.com>
2024-12-07 21:49:20 +0200

    Updated 2024 day 3 files

---
+++ y2024cpp/day3/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <format>
+#include <cmath>
+#include <array>
+#include <vector>
+#include "AdventOC.hpp"
+
+using namespace std::chrono;
+using std::cout, std::array, std::string;
+
+
+int main(){
+    const auto start_t = high_resolution_clock::now();
+
+    array<char, 128 * 1024> input;
+    advoc::readInput("../input.txt", &input[0], 128*1024);
+
+
+        
+    const auto end_t = high_resolution_clock::now();
+    duration<double, std::milli> elapsed_time{end_t - start_t};
+    cout << std::format("Task 1 solution: {}\n", "task1placeholder");
+    cout << std::format("Task 2 solution: {}\n", "task2placeholder");
+    cout << std::format("Time taken: {}", elapsed_time) << std::endl;
+    return 0;
+}
